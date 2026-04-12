842e73f4ec7443f90282ba6c5efba271da165508
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-04-12 19:29:30 +0700

    class peminjaman

---
+++ peminjaman.java
@@ -0,0 +1,31 @@
+public class peminjaman {
+
+    mahasiswa mhs;
+    buku buku;
+    int lamaPinjam;
+    int batasPinjam = 5;
+    int terlambat;
+    int denda;
+    
+    peminjaman(mahasiswa mhs, buku buku, int lamaPinjam) {
+        this.mhs = mhs;
+        this.buku = buku;
+        this.lamaPinjam = lamaPinjam;
+        hitungDenda();
+    }
+
+    void hitungDenda(){
+        if (lamaPinjam > batasPinjam) {
+            terlambat = lamaPinjam - batasPinjam;
+        } else {
+            terlambat = 0;
+        }
+
+        denda = terlambat * 2000;
+    }
+
+    void tampilPeminjaman() {
+        System.out.println(mhs.nim + " | " + mhs.nama + " | " + buku.judul + " | " + lamaPinjam + " hari | Terlambat: " + terlambat + " hari | Denda: " + denda);
+    }
+
+}
